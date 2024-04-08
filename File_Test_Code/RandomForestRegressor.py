ba791cbd0f6f31a3216a25d14f9ddbd1788c83c5
Viet0904 <viet0944840971@gmail.com>
2024-04-08 20:04:10 +0700

    Refactor code for DecisionTreeRegressor.ipynb and RandomForestRegressor.py

--- File_Test_Code/RandomForestRegressor.py
+++ File_Test_Code/RandomForestRegressor.py
@@ -12,7 +12,7 @@ warnings.filterwarnings("ignore")
 # pd.options.display.max_columns = 999
 
 # Đọc dữ liệu từ file "train.csv" và lưu vào DataFrame df
-df = pd.read_csv("train.csv")
+df = pd.read_csv("BikeSharingDemand.csv")
 
 # Tạo cột year, month, day, hour từ cột datetime
 df["datetime"] = pd.to_datetime(df["datetime"])
@@ -26,33 +26,72 @@ df = df.drop(columns=["datetime"])
 # Chia dữ liệu thành X và y
 X = df.drop(columns=["casual", "registered", "count"])
 print(X)
-y = np.log(df["count"])  # Áp dụng logarithm cho cột "count"
+y = df["count"]  # Áp dụng logarithm cho cột "count"
 
 # Chia dữ liệu thành tập huấn luyện và tập kiểm tra
-x_train, x_test, y_train, y_test = train_test_split(
+X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Xây dựng và huấn luyện mô hình Random Forest Regression
-model = RandomForestRegressor(n_estimators=1000, random_state=42)
-model.fit(x_train, y_train)
+# # Xây dựng và huấn luyện mô hình Random Forest Regression
+# model = RandomForestRegressor(n_estimators=1000, random_state=42)
+# model.fit(X_train, y_train)
 
-# Dự đoán kết quả trên tập kiểm tra
-y_pred = model.predict(x_test)
+# # Dự đoán kết quả trên tập kiểm tra
+# y_pred = model.predict(X_test)
 
-# Tính và in ra giá trị MSE và RMSE
-print("MSE = ", mean_squared_error(y_test, y_pred))
-print("RMSE = ", np.sqrt(mean_squared_error(y_test, y_pred)))
+# # Tính và in ra giá trị MSE và RMSE
+# print("MSE = ", mean_squared_error(y_test, y_pred))
+# print("RMSE = ", np.sqrt(mean_squared_error(y_test, y_pred)))
 
 
 from sklearn.model_selection import GridSearchCV
 
 # Định nghĩa lưới các tham số bạn muốn thử nghiệm
 param_grid = {
-    "n_estimators": [100, 500, 1000],  # Số lượng cây quyết định trong rừng
-    "max_depth": [None, 10, 20, 30],  # Độ sâu tối đa của các cây quyết định
-    "min_samples_split": [2, 5, 10],  # Số lượng mẫu tối thiểu để chia một nút
-    "min_samples_leaf": [1, 2, 4],  # Số lượng mẫu tối thiểu ở mỗi lá
+    "n_estimators": [
+        10,
+        20,
+        50,
+        100,
+        200,
+        500,
+        1000,
+    ],  # Số lượng cây quyết định trong rừng
+    "max_depth": [
+        None,
+        10,
+        20,
+        30,
+        50,
+        100,
+        200,
+        500,
+        1000,
+    ],  # Độ sâu tối đa của các cây quyết định
+    "min_samples_split": [
+        2,
+        5,
+        10,
+        20,
+        50,
+        100,
+        200,
+        500,
+        1000,
+    ],  # Số lượng mẫu tối thiểu để chia một nút
+    "min_samples_leaf": [
+        1,
+        2,
+        4,
+        10,
+        20,
+        50,
+        100,
+        200,
+        500,
+        1000,
+    ],  # Số lượng mẫu tối thiểu ở mỗi lá
 }
 
 # Tạo một đối tượng GridSearchCV với mô hình RandomForestRegressor và lưới tham số
@@ -62,14 +101,21 @@ grid_search = GridSearchCV(
 )
 
 # Tiến hành tìm kiếm trên lưới tham số sử dụng tập huấn luyện
-grid_search.fit(x_train, y_train)
+grid_search.fit(X_train, y_train)
 
 # In ra tham số tốt nhất đã tìm được
 print("Best parameters:", grid_search.best_params_)
 
 # Dự đoán kết quả trên tập kiểm tra sử dụng mô hình đã được điều chỉnh
-y_pred_grid = grid_search.predict(x_test)
+y_pred_grid = grid_search.predict(X_test)
 
 # Tính và in ra giá trị MSE và RMSE của mô hình tối ưu
 print("MSE (optimized) =", mean_squared_error(y_test, y_pred_grid))
 print("RMSE (optimized) =", np.sqrt(mean_squared_error(y_test, y_pred_grid)))
+
+plt.figure(figsize=(20, 15))
+plot_tree(
+    model, max_depth=3, feature_names=X.columns, filled=True, rounded=True, fontsize=8
+)
+plt.title("Decision Tree Regression (Max Depth = 10)", fontsize=16)
+plt.show()
